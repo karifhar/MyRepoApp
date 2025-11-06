ffa2634613d66693a13aa96503ee41f4a35035b9
RifqaHA <rifqa.akbar@iseller.id>
2025-11-06 14:23:56 +0700

    init commit

---
+++ MyRepoApps/Models/Abstracts/BaseEntity.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyRepoApps.Models.Abstract;
+
+public interface IHasKey
+{
+    Guid Id { get; set; }
+}
+
+public interface IModifiable
+{
+    DateTime? ModifiedAt { get; set; }
+    string? ModifiedBy { get; set; }
+}
+
+public interface IDeletable
+{
+    bool IsDeleted { get; set; }
+}
+public abstract class BaseEntity : IHasKey, IModifiable, IDeletable
+{
+    [Key]
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; } 
+    [MaxLength(255)]
+    public string CreatedBy { get; set; }
+    public DateTime? ModifiedAt { get; set; }
+    [MaxLength(255)]
+    public string? ModifiedBy { get; set; }
+    public bool IsDeleted { get; set; }
+}
---
+++ MyRepoApps/Models/MUser.cs
@@ -0,0 +1,8 @@
+﻿using MyRepoApps.Models.Abstract;
+
+namespace MyRepoApps.Models;
+
+public class MUser : BaseEntity
+{
+    public string MyProperty { get; set; }
+}
